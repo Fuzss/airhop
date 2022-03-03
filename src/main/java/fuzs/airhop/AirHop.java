12983fafd447cc066e64fdc46556e62d47a3ce25
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-03-03 21:12:52 +0100

    compiled for 1.18.2

--- src/main/java/fuzs/airhop/AirHop.java
+++ src/main/java/fuzs/airhop/AirHop.java
@@ -15,15 +15,15 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(AirHop.MOD_ID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class AirHop {
     public static final String MOD_ID = "airhop";
     public static final String MOD_NAME = "Air Hop";
-    public static final Logger LOGGER = LogManager.getLogger(AirHop.MOD_NAME);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     public static final NetworkHandler NETWORK = NetworkHandler.of(MOD_ID);
     @SuppressWarnings("Convert2MethodRef")
